8a00f4e5d08b96f25f762e5363e38ce97d6a7c9a
lovekushgupta1999 <guptalovekush64@gmail.com>
2025-01-16 21:27:33 +0530

    js

--- script.js
+++ script.js
@@ -182,22 +182,39 @@ async function edit(id){
     let res =await fetch(`http://localhost:3000/patient/${id}`);
     let data =await res.json();
     let edit_form=`
-    <input type="text" value="${data.id}" id="id1" readonly><br>
-      <select name="Dr. list" id="doctors1">
-                <option value="Dr. love">Dr. love</option>
-                <option value="Dr. kush">Dr. kush</option>
-                <option value="Dr. lovekush">Dr. lovekush1</option>
-                <option value="Dr. lovekush">Dr. lovekush2</option>
-                <option value="Dr. lovekush">Dr. lovekush3</option>
-                <option value="Dr. lovekush">Dr. lovekush4</option>
-              </select><br>
+    <h1 class="edittag">EDIT TABLE</h1>
+   <div class="form-container">
 
-    <input type="text" value="${data.name}" id="name1" ><br>
+    <label for="id1">ID</label>
+    <input type="text" value="${data.id}" id="id1" readonly><br>
+
+    <label for="doctors1">Doctor</label>
+    <select value="Dr. list" id="doctors1">
+        <option value="Dr. love">Dr. love</option>
+        <option value="Dr. kush">Dr. kush</option>
+        <option value="Dr. lovekush">Dr. lovekush1</option>
+        <option value="Dr. lovekush">Dr. lovekush2</option>
+        <option value="Dr. lovekush">Dr. lovekush3</option>
+        <option value="Dr. lovekush">Dr. lovekush4</option>
+    </select><br>
+
+    <label for="name1">Name</label>
+    <input type="text" value="${data.name}" id="name1"><br>
+
+    <label for="monumber1">Mobile Number</label>
     <input type="number" value="${data.monumber}" id="monumber1"><br>
+
+    <label for="age1">Age</label>
     <input type="text" value="${data.age}" id="age1"><br>
+
+    <label for="date1">Date</label>
     <input type="date" value="${data.date}" id="date1"><br>
-    <input type="time" value="${data.slot}" id="slot1"><br>
-<td><button id="updatebtn" onclick="myupdate('${data.id}')">Update</button></td>
+
+    <label for="slot_time">Time Slot</label>
+    <input type="time" value="${data.slot}" id="slot_time"><br>
+
+    <button id="updatebtn" onclick="myupdate('${data.id}')">Update</button>
+</div>
 
     `
     document.querySelector("#editform").innerHTML=edit_form
@@ -211,18 +228,20 @@ function myupdate(id){
         id: document.querySelector("#id1").value,
         doctor: document.querySelector("#doctors1").value,
         name: document.querySelector("#name1").value,
-
         monumber:document.getElementById("monumber1").value,
         age:document.getElementById("age1").value,
         date:document.getElementById("date1").value,
-        slot:document.getElementById("slot1").value
+        slot:document.querySelector('#slot_time').value
     }
+
+    console.info(update_data)
+
     fetch(`http://localhost:3000/patient/${id}`,{
         method:"PUT",
         headers:{
             "content-type":"application/json"
         },
-        body: JSON.stringify(update_data)
+        body:JSON.stringify(update_data)
     }).then(r=>alert("updated.........!!"))
 
     location.reload();
